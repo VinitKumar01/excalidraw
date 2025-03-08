0e0e22ff5cbcb6dba22b3b5e555f463c9b61582c
Vinit <vinitk81144@gmail.com>
2025-03-08 22:21:51 +0530

    routes completed, minor fixes

--- apps/http-backend/src/routes/route.ts
+++ apps/http-backend/src/routes/route.ts
@@ -8,17 +8,19 @@ import { authenticateUser } from "../middlewares";
 
 export const UserRouter: express.Router = Router();
 
-UserRouter.post("/signup", async (req: Request, res: Response) => {
-  const { error } = userSchema.safeParse(req.body);
+const signupSchema = userSchema.omit({ slug: true });
 
-  if (error) {
+UserRouter.post("/signup", async (req: Request, res: Response) => {
+  const { data, error, success } = signupSchema.safeParse(req.body);
+
+  if (error || !success) {
     res.status(411).json({
       error: error.format(),
     });
     return;
   }
 
-  const { username, email, password } = req.body;
+  const { username, email, password } = data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const userExists = await prismaClient.user.findFirst({
@@ -56,19 +58,19 @@ UserRouter.post("/signup", async (req: Request, res: Response) => {
   }
 });
 
-const signinSchema = userSchema.omit({ email: true });
+const signinSchema = userSchema.omit({ email: true, slug: true });
 
 UserRouter.post("/signin", async (req: Request, res: Response) => {
-  const { error } = signinSchema.safeParse(req.body);
+  const { data, error, success } = signinSchema.safeParse(req.body);
 
-  if (error) {
+  if (error || !success) {
     res.status(411).json({
       error: error.format(),
     });
     return;
   }
 
-  const { username, password } = req.body;
+  const { username, password } = data;
 
   try {
     const userExists = await prismaClient.user.findFirst({
@@ -109,3 +111,43 @@ UserRouter.post("/signin", async (req: Request, res: Response) => {
     });
   }
 });
+
+const roomSchema = userSchema.omit({
+  username: true,
+  email: true,
+  password: true,
+});
+
+UserRouter.post(
+  "/room",
+  authenticateUser,
+  async (req: Request, res: Response) => {
+    const { data, error, success } = roomSchema.safeParse(req.body);
+
+    if (error || !success) {
+      res.status(411).json({
+        error: error.format(),
+      });
+      return;
+    }
+
+    const { slug } = data;
+
+    try {
+      const room = await prismaClient.room.create({
+        data: {
+          slug,
+          admin: req.body.id,
+        },
+      });
+
+      res.json({
+        roomId: room.id,
+      });
+    } catch (_) {
+      res.status(411).json({
+        message: "Room already exists with this name",
+      });
+    }
+  }
+);
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,21 +1,32 @@
 import * as z from "zod";
 
 export const userSchema = z.object({
-    email: z
-        .string({ required_error: "Email must be provided" })
-        .email({ message: "Invalid Email Format" }),
-    username: z
-        .string({ required_error: "Username must be provided" })
-        .min(3, { message: "Username must be 3 letters or more" })
-        .max(20, { message: "Username must be less than 20 characters" })
-        .regex(/^[a-zA-Z0-9_]+$/, {
-            message: "Username can only contain letters, numbers, and underscores",
-        }),
-    password: z
-        .string({ required_error: "Password must be provided" })
-        .min(8, { message: "Password must be 8 letters or characters" })
-        .regex(/^(?=.*[a-zA-Z])(?=.*[\d\W])[a-zA-Z\d\W]{8,}$/, {
-            message:
-                "Password must include 8+ chars, at least 1 letter, 1 special char or number",
-        }),
+  email: z
+    .string({ required_error: "Email must be provided" })
+    .email({ message: "Invalid Email Format" })
+    .trim(),
+  username: z
+    .string({ required_error: "Username must be provided" })
+    .min(3, { message: "Username must be 3 letters or more" })
+    .max(20, { message: "Username must be less than 20 characters" })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message: "Username can only contain letters, numbers, and underscores",
+    })
+    .trim(),
+  password: z
+    .string({ required_error: "Password must be provided" })
+    .min(8, { message: "Password must be 8 letters or characters" })
+    .regex(/^(?=.*[a-zA-Z])(?=.*[\d\W])[a-zA-Z\d\W]{8,}$/, {
+      message:
+        "Password must include 8+ chars, at least 1 letter, 1 special char or number",
+    })
+    .trim(),
+  slug: z
+    .string({ required_error: "Room name must be provided" })
+    .min(3, { message: "Room name must be 3 letters or more" })
+    .max(20, { message: "Room name must be less than 20 characters" })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message: "Room name can only contain letters, numbers, and underscores",
+    })
+    .trim(),
 });
